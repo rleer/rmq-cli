2068fbbe8d5af13bc38c5496240ed86dfc29ee4a
Richard Leer <richard.leer@parshipgroup.com>
2025-10-08 15:33:39 +0200

    Remove json path as output format

--- src/RmqCli/ConsumeCommand/MessageFormatter/MessageFormatterFactory.cs
+++ src/RmqCli/ConsumeCommand/MessageFormatter/MessageFormatterFactory.cs
@@ -23,7 +23,6 @@ public class MessageFormatterFactory : IMessageFormatterFactory
             OutputFormat.Plain => _formatters.First(formatter => formatter is TextMessageFormatter),
             OutputFormat.Table => throw new NotImplementedException("Table formatter is not yet implemented"),
             OutputFormat.Json => _formatters.First(formatter => formatter is JsonMessageFormatter),
-            OutputFormat.JsonPath => throw new NotImplementedException("JSON path formatter is not yet implemented"),
             _ => throw new ArgumentOutOfRangeException(nameof(format), format, "Unknown output format")
         };
     }
